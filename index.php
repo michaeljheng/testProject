97da60e09fdc334f4c374bd5fc96e1fbc92d2a9a
MichaelJheng <MichaelJheng@MichaelJheng-PC>
2017-05-11 21:30:35 +0800

    2017/5/11 index.php 控制ESP8266中的GPIO_2,來使LED亮與滅,此程式使用AJAX之POST來發送指令給 ESP8266

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-echo "Hello.World!!";
+<html>
+	<head>
+		<title>AJAX</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<script src="./jquery/jquery-3.2.1.min.js"></script>
+		<script>
+		$(document).ready(function () {
+			$("#LED_SW").click(function () {
+				var SW = $("input[name='SW']:checked").val();
+				$.ajax({
+					url: "http://192.168.0.103/sw",
+					method: 'POST',
+					data: { led: SW },
+					dataType: "json",
+					success: function (data, status) {
+						alert("数据：" + data.data + "\n状态：" + status);
+					}
+				});
+			});
+		});
+		</script>		
+		
+	</head>
+	<body>
+		<div id="LED_SW">
+			<input type="radio" id="LED_ON" class="SW" value="on" name="SW">
+				<label for="LED_ON">開</label>
+			<input type="radio" id="LED_OFF" class="SW" value="off" name="SW" checked="checked">
+				<label for="LED_OFF">關</label>				
+		</div>
+	</body>
+</html>
